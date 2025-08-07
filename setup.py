4d92f51470b0749d48d9e509c7a41442c3db4528
muhammadshahalc <shahalhere@gmail.com>
2025-08-07 16:38:10 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "ai-rag-medbot",
+    version = "0.1.0",
+    author="muhammadshahalc",
+    author_email="shahalhere@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
